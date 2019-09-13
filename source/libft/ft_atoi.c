3f894a9241aa250e4a0a729befdcbd78c2edb7ca
Tshireletso MENTOR <tmentor@c4r19s4.wethinkcode.co.za>
2019-09-13 07:15:51 +0200

    added

--- source/libft/ft_atoi.c
+++ source/libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: tmentor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 11:11:35 by tmentor           #+#    #+#             */
-/*   Updated: 2019/05/31 15:48:37 by tmentor          ###   ########.fr       */
+/*   Updated: 2019/09/13 07:14:27 by tmentor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int			ft_atoi(const char *str)
 	i = 0;
 	nbr = 0;
 	sign = 1;
+	if (!str)
+		return (0);
 	while (str[i] == '\t' || str[i] == '\v' || str[i] == '\n' ||
 					str[i] == '\r' || str[i] == '\f' || str[i] == ' ')
 		i++;
@@ -33,5 +35,8 @@ int			ft_atoi(const char *str)
 		nbr = nbr * 10 + (str[i] - '0');
 		i++;
 	}
-	return (nbr * sign);
+	nbr *= sign;
+	if (nbr > 2147483647 || nbr < -2147483648)
+			return (0);
+	return (nbr);
 }
